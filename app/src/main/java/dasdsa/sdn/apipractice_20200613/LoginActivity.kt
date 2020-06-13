164d21cad7bfd6cc940ce4948c1de0d6d78c0c6a
umgonji <umgonji@naver.com>
2020-06-13 11:05:53 +0900

    로그인 버튼에 이벤트 추가  - 서버에 아이디 / 비번을 가지고 로그인 시도 요청 예정

--- app/src/main/java/dasdsa/sdn/apipractice_20200613/LoginActivity.kt
+++ app/src/main/java/dasdsa/sdn/apipractice_20200613/LoginActivity.kt
@@ -1,6 +1,7 @@
 package dasdsa.sdn.apipractice_20200613
 
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,6 +12,14 @@ class LoginActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+
+        loginBtn.setOnClickListener {
+            val inputEmail = emailEdt.text.toString()
+            val inputPw = pwEdt.text.toString()
+
+            //실제로 서버에 두개의 변수를 전달해서 로그인 시도
+
+        }
     }
 
     override fun setValues() {
