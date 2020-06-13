6c6d8d8bf08c8f3fc72a10b9a6d64ecb97f9cb6b
umgonji <umgonji@naver.com>
2020-06-13 13:44:47 +0900

    회원가입 화면 구현중  - 이메일 중복체크만 우선 진행

---
+++ app/src/main/java/dasdsa/sdn/apipractice_20200613/SingUpActivity.kt
@@ -0,0 +1,21 @@
+package dasdsa.sdn.apipractice_20200613
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SingUpActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sing_up)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+    }
+
+    override fun setValues() {
+    }
+
+
+}
