8c82bf7707e8cb319db038d35222a526542c6662
umgonji <umgonji@naver.com>
2020-06-13 14:12:55 +0900

    이메일 중복체크 마무리

--- app/src/main/java/dasdsa/sdn/apipractice_20200613/SingUpActivity.kt
+++ app/src/main/java/dasdsa/sdn/apipractice_20200613/SingUpActivity.kt
@@ -22,6 +22,18 @@ class SingUpActivity : BaseActivity() {
             ServerUtil.getRequestDuplicatedCheck(mContext, "EMAIL", inputEmail, object : ServerUtil.JsonResponseHandler {
                 override fun onResponse(json: JSONObject) {
 
+                    val code = json.getInt("code")
+                    if( code == 200 ){
+                        runOnUiThread {
+                            emailCheckResultTxt.text = "사용해도 좋습니다."
+                        }
+
+                    }
+                    else {
+                        runOnUiThread {
+                            emailCheckResultTxt.text = "이미 사용중입니다. 다른 이메일로 다시 체크해주세요."
+                        }
+                    }
                 }
             })
 
