22b5f0cef7a986a7a83588967db5ba1f9a95817c
umgonji <umgonji@naver.com>
2020-06-13 14:41:47 +0900

    닉네임 중복확인 기능 완료

--- app/src/main/java/dasdsa/sdn/apipractice_20200613/SingUpActivity.kt
+++ app/src/main/java/dasdsa/sdn/apipractice_20200613/SingUpActivity.kt
@@ -2,6 +2,7 @@ package dasdsa.sdn.apipractice_20200613
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import dasdsa.sdn.apipractice_20200613.utils.ServerUtil
 import kotlinx.android.synthetic.main.activity_sing_up.*
 import org.json.JSONObject
@@ -15,6 +16,29 @@ class SingUpActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+
+        nickNameCheckBtn.setOnClickListener {
+            val inputNick = nickNameEdt.text.toString()
+
+            ServerUtil.getRequestDuplicatedCheck(mContext, "NICKNAME", inputNick, object : ServerUtil.JsonResponseHandler{
+                override fun onResponse(json: JSONObject) {
+                    val code = json.getInt("code")
+                    if(code == 200) {
+                        runOnUiThread { 
+                            nickNameCheckResultTxt.text = "사용해도 좋습니다."
+                        }
+                    }
+                    else {
+                        runOnUiThread {
+                            nickNameCheckResultTxt.text = "이미 사용중입니다. 다른 닉네임으로 다시 체크해주세요."
+                        }
+                    }
+
+                }
+            })
+        }
+
+
         emailCheckBtn.setOnClickListener {
             //입력한 이메일이 이미 회원으로 있는지 확인 => 서버에 요청
             val inputEmail = emailEdt.text.toString()
@@ -27,7 +51,6 @@ class SingUpActivity : BaseActivity() {
                         runOnUiThread {
                             emailCheckResultTxt.text = "사용해도 좋습니다."
                         }
-
                     }
                     else {
                         runOnUiThread {
@@ -36,8 +59,6 @@ class SingUpActivity : BaseActivity() {
                     }
                 }
             })
-
-
         }
     }
 
