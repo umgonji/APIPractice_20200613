21fdaed6c597a46f7d035f284a49c768668622bb
umgonji <umgonji@naver.com>
2020-06-13 14:51:36 +0900

    입력값을 변경하면 다시 중복검사를 요구

--- app/src/main/java/dasdsa/sdn/apipractice_20200613/SingUpActivity.kt
+++ app/src/main/java/dasdsa/sdn/apipractice_20200613/SingUpActivity.kt
@@ -2,6 +2,9 @@ package dasdsa.sdn.apipractice_20200613
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import android.widget.Toast
 import dasdsa.sdn.apipractice_20200613.utils.ServerUtil
 import kotlinx.android.synthetic.main.activity_sing_up.*
@@ -16,6 +19,31 @@ class SingUpActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+
+        emailEdt.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                //글자가 변경된 시점에 실행되는 함수
+//                Log.d("변경된 내용", s.toString())
+                //이메일 중복검사 하라고 안내
+                emailCheckResultTxt.text = "중복확인을 해주세요."
+            }
+        })
+
+        nickNameEdt.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+            }
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                nickNameCheckResultTxt.text = "중복확인을 해주세요."
+            }
+        })
 
         nickNameCheckBtn.setOnClickListener {
             val inputNick = nickNameEdt.text.toString()
@@ -33,7 +61,6 @@ class SingUpActivity : BaseActivity() {
                             nickNameCheckResultTxt.text = "이미 사용중입니다. 다른 닉네임으로 다시 체크해주세요."
                         }
                     }
-
                 }
             })
         }
