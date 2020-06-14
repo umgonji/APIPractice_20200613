64c95e94d8a22e90e567d1714d0d16482125a5b9
umgonji <umgonji@naver.com>
2020-06-14 16:18:34 +0900

    댓글 파싱 코드 추가중  => 작성자를 추가로 파싱해야하는 상황

--- app/src/main/java/dasdsa/sdn/apipractice_20200613/datas/TopicReply.kt
+++ app/src/main/java/dasdsa/sdn/apipractice_20200613/datas/TopicReply.kt
@@ -7,7 +7,17 @@ class TopicReply {
     companion object {
         fun getTopicReplyFromJson(json: JSONObject) : TopicReply {
             val tr = TopicReply()
-
+            
+            tr.id = json.getInt("id")
+            tr.content = json.getString("content")
+            tr.topicId = json.getInt("topic_id")
+            tr.sideId = json.getInt("side_id")
+            tr.userId = json.getInt("user_id")
+            
+            
+            // tr.user = ?? //의견 JSON의 항목중 user JSONObject를 => User클래스에 전달
+            //User클래스가 Json을 받아서 => uSR로 변환해서 리턴.
+            //tr.user 에 대입
 
             return  tr
         }
