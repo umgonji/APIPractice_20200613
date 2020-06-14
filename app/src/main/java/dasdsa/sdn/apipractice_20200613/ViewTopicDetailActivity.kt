b95eca77ffaff6422da9724a9512d87e95c0b7b7
umgonji <umgonji@naver.com>
2020-06-14 15:01:44 +0900

    선택 진영 정보도 출력 (ViewTopicDetailActivity 에 추가)  => 주제에 같이 파싱된 진영 정보를 활용만 해서 출력

--- app/src/main/java/dasdsa/sdn/apipractice_20200613/ViewTopicDetailActivity.kt
+++ app/src/main/java/dasdsa/sdn/apipractice_20200613/ViewTopicDetailActivity.kt
@@ -59,6 +59,10 @@ class ViewTopicDetailActivity : BaseActivity() {
                         topicTitleTxt.text = mTopic.title
 
                         Glide.with(mContext).load(mTopic.imageUrl).into(topicImg)
+
+                        //선택 진영 정보도 출력
+                        firstSideTxt.text = mTopic.sides[0].title
+                        secondSideTxt.text = mTopic.sides[1].title
                     }
 
                 }
