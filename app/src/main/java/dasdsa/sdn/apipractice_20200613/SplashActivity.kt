d5b9bbc4e37a88c1688ee70346413d0ceb6e2131
umgonji <umgonji@naver.com>
2020-06-14 11:24:21 +0900

    3초 후에 로그인 화면 이동

--- app/src/main/java/dasdsa/sdn/apipractice_20200613/SplashActivity.kt
+++ app/src/main/java/dasdsa/sdn/apipractice_20200613/SplashActivity.kt
@@ -1,7 +1,9 @@
 package dasdsa.sdn.apipractice_20200613
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class SplashActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,6 +18,14 @@ class SplashActivity : BaseActivity() {
     }
 
     override fun setValues() {
+        // 3초 뒤에 다음화면으로 이동
+        val myHandler = Handler()
+        myHandler.postDelayed({
+            //시간이 지난뒤 실행할 내용
+            val myIntent = Intent(mContext, LoginActivity::class.java)
+            startActivity(myIntent)
+            finish()
+        }, 3000 )
 
     }
 
