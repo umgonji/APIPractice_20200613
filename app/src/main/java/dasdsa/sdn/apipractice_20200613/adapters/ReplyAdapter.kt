be390490c5a7bb7be465268f1f6700e75a8490ac
umgonji <umgonji@naver.com>
2020-06-14 16:06:19 +0900

    의견 목록 어뎁터 뼈대작업

---
+++ app/src/main/java/dasdsa/sdn/apipractice_20200613/adapters/ReplyAdapter.kt
@@ -0,0 +1,30 @@
+package dasdsa.sdn.apipractice_20200613.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import dasdsa.sdn.apipractice_20200613.R
+import dasdsa.sdn.apipractice_20200613.datas.TopicReply
+
+class ReplyAdapter(
+    val mContext:Context,
+    val resId:Int,
+    val mList:List<TopicReply>) : ArrayAdapter<TopicReply>(mContext, resId, mList) {
+
+    val inf = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var tempRow = convertView
+
+        tempRow?.let {
+
+        }.let {
+            tempRow = inf.inflate(R.layout.topic_reply_list_item, null)
+        }
+        val row = tempRow!!
+
+        return row
+    }
+}
