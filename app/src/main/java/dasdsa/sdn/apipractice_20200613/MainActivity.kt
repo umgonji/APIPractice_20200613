3a4ae892f9b15e5da5ad1dadbd02053da40ac2ad
umgonji <umgonji@naver.com>
2020-06-14 12:09:59 +0900

    토론 주제 받아오기 준비 작업중  => 리스트뷰 / 데이터클래스 / list_item XML

--- app/src/main/java/dasdsa/sdn/apipractice_20200613/MainActivity.kt
+++ app/src/main/java/dasdsa/sdn/apipractice_20200613/MainActivity.kt
@@ -16,7 +16,7 @@ class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setValues()
+        setupEvents()
         setValues()
     }
 
---
+++ app/src/main/java/dasdsa/sdn/apipractice_20200613/datas/Topic.kt
@@ -0,0 +1,10 @@
+package dasdsa.sdn.apipractice_20200613.datas
+
+class Topic {
+
+    var id = 0
+    var title = ""
+    var imageUrl = ""
+
+
+}
