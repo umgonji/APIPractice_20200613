f0c6751314df451aeef542018526c1d4373e314a
umgonji <umgonji@naver.com>
2020-06-20 10:42:40 +0900

    의견 작성 화면 그리기

---
+++ app/src/main/java/dasdsa/sdn/apipractice_20200613/EditReplyActivity.kt
@@ -0,0 +1,23 @@
+package dasdsa.sdn.apipractice_20200613
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditReplyActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_reply)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
+
+}
