4db10eae0c8d246f03254561a783fc4f840c6cc8
umgonji <umgonji@naver.com>
2020-06-20 13:35:39 +0900

    실제 좋아요/싫어요/답글 갯수 출력

--- app/src/main/java/dasdsa/sdn/apipractice_20200613/adapters/ReplyAdapter.kt
+++ app/src/main/java/dasdsa/sdn/apipractice_20200613/adapters/ReplyAdapter.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.TextView
 import dasdsa.sdn.apipractice_20200613.R
 import dasdsa.sdn.apipractice_20200613.datas.TopicReply
@@ -28,10 +29,10 @@ class ReplyAdapter(
 
         //xml에서 사용할 뷰 가져오기
         val writerNickNameTxt = row.findViewById<TextView>(R.id.writerNickNameTxt)
-
-
-
         val contentTxt = row.findViewById<TextView>(R.id.contentTxt)
+        val replyBtn = row.findViewById<Button>(R.id.replyBtn)
+        val likeBtn = row.findViewById<Button>(R.id.likeBtn)
+        val dislikeBtn = row.findViewById<Button>(R.id.dislikeBtn)
 
         //목록에서 뿌려줄 데이터 꺼내오기
         val data = mList[position]
@@ -39,6 +40,10 @@ class ReplyAdapter(
         //데이터 / 뷰 연결 => 알고리즘 고민하는곳
         writerNickNameTxt.text = data.user.nickName
         contentTxt.text = data.content
+
+        replyBtn.text = "답글 : ${data.replyCount}"
+        likeBtn.text = "좋아요 : ${data.likeCount}"
+        dislikeBtn.text = "좋아요 : ${data.dislikeCount}"
 
         return row
     }
--- app/src/main/java/dasdsa/sdn/apipractice_20200613/datas/TopicReply.kt
+++ app/src/main/java/dasdsa/sdn/apipractice_20200613/datas/TopicReply.kt
@@ -21,6 +21,12 @@ class TopicReply {
 
             val userJson = json.getJSONObject("user")
             tr.user = User.getUserFromJson(userJson)
+            
+            // 답글 / 좋아요 / 싫어요 갯수 파싱
+            tr.replyCount = json.getInt("reply_count")
+            tr.likeCount = json.getInt("like_count")
+            tr.dislikeCount = json.getInt("dislike_count")
+
 
             return  tr
         }
@@ -33,6 +39,11 @@ class TopicReply {
     var userId = 0
 
     lateinit var user : User
+    
+    // 답글 / 좋아요 / 싫어요 갯수 저장 변수
+    var replyCount = 0
+    var likeCount = 0
+    var dislikeCount = 0
 
 
 }
