4e20cfe7d7db3dac2322fe4146b4938f012cf120
umgonji <umgonji@naver.com>
2020-06-21 10:18:38 +0900

    답글 버튼 눌러서 들어올 화면 초안 그리는 중

---
+++ app/src/main/java/dasdsa/sdn/apipractice_20200613/ViewReplyDetailActivity.kt
@@ -0,0 +1,23 @@
+package dasdsa.sdn.apipractice_20200613
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewReplyDetailActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_reply_detail)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
+
+}
